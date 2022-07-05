60306105d172da059f4da169a1e0cfd796b22b28
Lavr <avatar_str@list.ru>
2022-07-06 00:14:31 +0500

    fix: update data

--- data.ts
+++ data.ts
@@ -16,8 +16,88 @@ export const tickets: Ticket[] = [
         origin: "MOW",
         destination: "HKT",
         date: "01.06.2022",
+        stops: ["HKG"],
+        duration: (13 * 60 + 30) * 60 * 1000,
+      },
+    ],
+  },
+  {
+    price: 13400,
+    carrier: "S7 Airlines",
+    segments: [
+      {
+        origin: "MOW",
+        destination: "HKT",
+        date: "01.06.2022",
         stops: ["HKG", "JNB"],
         duration: (21 * 60 + 15) * 60 * 1000,
+      },
+      {
+        origin: "MOW",
+        destination: "HKT",
+        date: "01.06.2022",
+        stops: ["HKG"],
+        duration: (13 * 60 + 30) * 60 * 1000,
+      },
+    ],
+  },
+  {
+    price: 13400,
+    carrier: "S7 Airlines",
+    segments: [
+      {
+        origin: "MOW",
+        destination: "HKT",
+        date: "01.06.2022",
+        stops: ["HKG", "JNB"],
+        duration: (21 * 60 + 15) * 60 * 1000,
+      },
+      {
+        origin: "MOW",
+        destination: "HKT",
+        date: "01.06.2022",
+        stops: ["HKG"],
+        duration: (13 * 60 + 30) * 60 * 1000,
+      },
+    ],
+  },
+  {
+    price: 13400,
+    carrier: "S7 Airlines",
+    segments: [
+      {
+        origin: "MOW",
+        destination: "HKT",
+        date: "01.06.2022",
+        stops: ["HKG", "JNB"],
+        duration: (21 * 60 + 15) * 60 * 1000,
+      },
+      {
+        origin: "MOW",
+        destination: "HKT",
+        date: "01.06.2022",
+        stops: ["HKG"],
+        duration: (13 * 60 + 30) * 60 * 1000,
+      },
+    ],
+  },
+  {
+    price: 13400,
+    carrier: "S7 Airlines",
+    segments: [
+      {
+        origin: "MOW",
+        destination: "HKT",
+        date: "01.06.2022",
+        stops: ["HKG", "JNB"],
+        duration: (21 * 60 + 15) * 60 * 1000,
+      },
+      {
+        origin: "MOW",
+        destination: "HKT",
+        date: "01.06.2022",
+        stops: ["HKG"],
+        duration: (13 * 60 + 30) * 60 * 1000,
       },
     ],
   },
