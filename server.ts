5fe9c9b03d3bf786c2cebaf6d2a4d9f43273b419
Lavr <avatar_str@list.ru>
2022-07-06 00:12:26 +0500

    fix: Добавление cors. Фикс ошибки в response.

--- server.ts
+++ server.ts
@@ -5,17 +5,24 @@ const fastify = Fastify({
   logger: true,
 });
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+fastify.register(require("@fastify/cors"));
+
 const response = () => {
   return new Promise((res, rej) => {
     setTimeout(() => {
-      return Math.random() > 0.3 ? res(tickets) : rej([]);
+      return Math.random() > 0.5 ? res(tickets) : rej("no data");
     }, 3000);
   });
 };
 
 fastify.get("/api/tickets", async (request, reply) => {
-  const data = await response();
-  reply.send(JSON.stringify(data));
+  try {
+    const data = await response();
+    reply.send(JSON.stringify(data));
+  } catch (error) {
+    reply.status(500).send({ status: 500, message: error });
+  }
 });
 
 fastify.listen({ port: 3001 }, (err, address) => {
